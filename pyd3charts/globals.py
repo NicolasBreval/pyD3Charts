87e1913cc5528008e4e43e6ccc38fd8c7acee316
NicolasBreval <nicolasbrevalrodriguez@gmail.com>
2020-07-02 14:32:42 +0200

    ADD - Global variables module for templates and files searching

---
+++ pyd3charts/globals.py
@@ -0,0 +1,14 @@
+import os
+
+# Files folders
+TEMPLATES_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+SCRIPTS_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'scripts')
+STYLES_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'styles')
+EXTERNAL_SCRIPTS_FOLDER = os.path.join(SCRIPTS_FOLDER, 'external')
+CHARTS_SCRIPTS_FOLDER = os.path.join(SCRIPTS_FOLDER, 'charts')
+
+JINJA_DEPENDENCIES = [TEMPLATES_FOLDER, EXTERNAL_SCRIPTS_FOLDER, STYLES_FOLDER, CHARTS_SCRIPTS_FOLDER]
+
+# Files names
+BASE_TEMPLATE = 'base.html'
+SCATTER = 'scatter.js'
